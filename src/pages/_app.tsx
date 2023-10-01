de9fff4a99633381ec7a7968155b3aea79d85cd7
HaithamBH <haitham.benhammouda@gmail.com>
2023-10-01 13:10:01 +0100

    import styles and components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,13 @@
+import '@/styles/home.css'
 import '@/styles/globals.css'
+import '@/styles/about.css'
+import '@/styles/contact.css'
+import '@/styles/globals.css'
+import '@/styles/navbar.css'
+import '@/styles/projects.css'
+import '@/styles/stage.css'
+import '@/styles/footer.scss'
+import '@/styles/projectCard.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
