6de7273bf90d23810cfd677a2f2ca160870a03fb
HaithamBH <haitham.benhammouda@gmail.com>
2023-10-14 20:56:02 +0100

    meta update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>
+          Haitham BenHammouda Portfolio
+        </title>
+        <meta name="description" content="Know more about Haitham as a MERN and Golang developer" />
+        <meta name='keywords' content='Haitham Benhammouda, Haithem Benhammouda, MERN, Golang, Go developer'/>
+        <meta name='author' content='Haithem Benhammouda'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
